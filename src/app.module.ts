efd4576ca0f96b98321cc5dfbd186ff48cfefad2
tainara-anjos <taisenac25@gmail.com>
2026-02-23 10:40:26 -0300

    tarefa-08

---
+++ src/app.module.ts
@@ -0,0 +1,12 @@
+import { UsersModule } from './users/users.module';
+import { DatabaseService } from './database/database.service';
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
+@Module({
+imports: [UsersModule],
+  controllers: [AppController],
+  providers: [AppService, DatabaseService],
+})
+export class AppModule {}
